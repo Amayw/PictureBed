9107b30ab6e25794950b5f39b684b7feb0b78e47
wywang <727936199@qq.com>
2020-11-30 13:19:45 +0800

    完成注册页面输入验证

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,36 +1,20 @@
-import React,{useRef} from 'react';
-import {observer} from 'mobx-react'
-import {useStores} from '../stores'
-import { Form, Input, Button, Checkbox } from 'antd';
+import React from 'react';
 import styled from 'styled-components';
-// const Login=observer(
-//         ()=> {
-//         const {AuthStore}=useStores();
-//         const inputRef=useRef();
-//
-//         const bindChange = e => {
-//             AuthStore.setUsername(inputRef.current.value)
-//         }
-//
-//         return (
-//             <>
-//                 <h1>Login:{AuthStore.values.username}</h1>
-//                 <input onChange={bindChange} ref={inputRef}/>
-//             </>
-//         );
-//     }
-// )
-const FormWrapper=styled.div`
-    border: 1px solid red;
-    padding-top:100px ;
-`
+import {Button, Form, Input} from 'antd';
+
+const FormWrapper = styled.div`
+    padding-top:50px ;
+    >h1{
+      text-align: center;
+    }
+`;
 
 const layout = {
-    labelCol: { span: 8 },
-    wrapperCol: { span: 10 },
+    labelCol: {span: 8},
+    wrapperCol: {span: 8},
 };
 const tailLayout = {
-    wrapperCol: { offset: 8, span: 10 },
+    wrapperCol: {offset: 11, span: 2},
 };
 
 const Login = () => {
@@ -42,33 +26,49 @@ const Login = () => {
         console.log('Failed:', errorInfo);
     };
 
+    const validateUserName=(rule,value)=>{
+        if(/\W/.test(value)) return Promise.reject('不能出现字母数字下划线以外的字符')
+        if(value.length<4||value.length>10) return Promise.reject('长度外4到10个字符')
+        return Promise.resolve();
+    }
+
     return (
         <FormWrapper>
+            <h1>登录</h1>
             <Form
                 {...layout}
                 name="basic"
-                initialValues={{ remember: true }}
                 onFinish={onFinish}
                 onFinishFailed={onFinishFailed}
             >
                 <Form.Item
                     label="Username"
                     name="username"
-                    rules={[{ required: true, message: 'Please input your username!' }]}
+                    rules={[{
+                        required: true,
+                        message: 'Please input your username!'
+                    },
+                        {
+                            validator:validateUserName
+                        }]
+                    }
                 >
-                    <Input />
+                    <Input/>
                 </Form.Item>
 
                 <Form.Item
                     label="Password"
                     name="password"
-                    rules={[{ required: true, message: 'Please input your password!' }]}
+                    rules={[{
+                        required: true,
+                        message: 'Please input your password!'
+                    },
+                        {
+                            min: 6,
+                            message: 'password must be at least 6 characters'
+                        }]}
                 >
-                    <Input.Password />
-                </Form.Item>
-
-                <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-                    <Checkbox>Remember me</Checkbox>
+                    <Input.Password/>
                 </Form.Item>
 
                 <Form.Item {...tailLayout}>
--- src/views/Register.js
+++ src/views/Register.js
@@ -1,15 +1,102 @@
 import React from 'react';
-import {observer} from 'mobx-react'
-import {useStores} from '../stores';
+import styled from 'styled-components';
+import {Button, Form, Input} from 'antd';
 
-const Register=observer(()=>{
-    const {AuthStore}=useStores();
+const FormWrapper = styled.div`
+    width: 40vw;
+    margin: 100px auto;
+    
+`;
+
+const layout = {
+    labelCol: {span: 4},
+    // wrapperCol: {span: 8},
+};
+const tailLayout = {
+    wrapperCol: {offset: 12},
+};
+
+const Register = () => {
+    const onFinish = values => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = errorInfo => {
+        console.log('Failed:', errorInfo);
+    };
+
+    const validateUserName=(rule,value)=>{
+        if(/\W/.test(value)) return Promise.reject('不能出现字母数字下划线以外的字符')
+        if(value.length<4||value.length>10) return Promise.reject('长度外4到10个字符')
+        return Promise.resolve();
+    }
+
+    const validateConfirm=({getFieldValue})=>({
+        validator(rule,value){
+            if(getFieldValue('password')===value) return Promise.resolve();
+            return Promise.reject('两次输入密码不一致')
+    }
+    })
+
     return (
-        <footer>
-            <h1>Register</h1>
-            {AuthStore.values.username}
-        </footer>
+        <FormWrapper>
+            <Form
+                {...layout}
+                name="basic"
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+            >
+                <Form.Item
+                    label="用户名"
+                    name="username"
+                    rules={[{
+                        required: true,
+                        message: '请输入用户名!'
+                    },
+                    {
+                        validator:validateUserName
+                    }]
+                    }
+                >
+                    <Input/>
+                </Form.Item>
+
+                <Form.Item
+                    label="密码"
+                    name="password"
+                    rules={[{
+                        required: true,
+                        message: '请输入密码！'
+                    },
+                    {
+                        min: 6,
+                        message: '长度不少于6个字符'
+                    }]}
+                >
+                    <Input.Password/>
+                </Form.Item>
+
+                <Form.Item
+                    label="确认密码"
+                    name="confirmPassword"
+                    rules={[{
+                        required: true,
+                        message: '请确认密码!'
+                    },
+                     validateConfirm
+                    ]}
+                >
+                    <Input.Password/>
+                </Form.Item>
+
+                <Form.Item {...tailLayout}>
+                    <Button type="primary" htmlType="submit">
+                        注册
+                    </Button>
+                </Form.Item>
+            </Form>
+        </FormWrapper>
     );
-})
+};
 
 export default Register;
