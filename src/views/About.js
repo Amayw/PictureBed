ee493437c40403fb6431c8da8ca8d187a30a255a
wywang <727936199@qq.com>
2020-12-03 16:47:31 +0800

    完善About组件和README.md

--- src/views/About.js
+++ src/views/About.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import styled from 'styled-components'
 
+const AboutWrapper=styled.div`
+    >div{
+
+    }
+  
+
+`
 function About() {
     return (
-        <>
-            <h1>About</h1>
-        </>
+        <AboutWrapper>
+                <h1>蓝胖子图床</h1>
+                <div>这是我用React实现的一个图床项目，上传图片前请记得先登录或注册。</div>
+                <div>登录后把需要上传的图片拖拽至哆啦A梦的嘴巴或者直接点击哆啦A梦的嘴巴即可上传。</div>
+                <div>在上传历史中，可以查看所有上传的图片。</div>
+                <div>尽管做了移动端适配，但还是建议PC端使用，体验更<span>佳</span>哦~</div>
+        </AboutWrapper>
     );
 }
 
